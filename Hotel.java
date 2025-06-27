8b63fcc5e1d3d01a9209a8c5fe3c5085f3f6c64d
Arul <s.arulprasanth2004@gmail.com>
2025-06-27 11:30:11 +0530

    structureofjava

---
+++ Hotel.java
@@ -0,0 +1,21 @@
+public class Hotel {
+    
+    int coffee = 20;
+    int tea = 10;
+
+    public static void main(String[] args) {
+
+        Hotel server1 = new Hotel();
+        System.out.println(server1.coffee);
+        Hotel server2 = new Hotel();
+        System.out.println(server2.tea);
+        
+    }
+}
+
+
+/*
+output   
+20   
+10   
+*/ 
